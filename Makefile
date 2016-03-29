53326e98d37c2ae42b3e4e15712b6bc8a70c8126
Elliott Mantock <emantock@gmail.com>
2016-03-29 10:13:49 -0400

    implemented start of junit tests

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 JDKPATH = /usr
 LIBPATH = ./lib/bufmgr.jar:./lib/diskmgr.jar:./lib/heap.jar:./lib/index.jar:./lib/relop.jar
+JUNITPATH = /usr/share/java/junit4.jar
 
-CLASSPATH = .:..:$(LIBPATH)
+CLASSPATH = .:..:$(LIBPATH):$(JUNITPATH)
 BINPATH = $(JDKPATH)/bin
 JAVAC = $(JDKPATH)/bin/javac 
 JAVA  = $(JDKPATH)/bin/java 
@@ -15,7 +16,10 @@ compile:src/*/*.java
 	$(JAVAC) -cp $(CLASSPATH) -d ./bin ./src/*/*.java
 
 xx : compile
-	$(JAVA) -cp $(CLASSPATH):./bin global.Msql tests/queries.sql
+	$(JAVA) -cp $(CLASSPATH):./bin global.Msql queries/queries.sql
+
+test: compile
+	$(JAVA) -cp $(CLASSPATH):./bin org.junit.runner.JUnitCore tests.MinibaseTest
 
 clean:
 	rm -rf *.minibase bin
---
+++ src/tests/MinibaseTest.java
@@ -0,0 +1,75 @@
+package tests;
+
+import global.Minibase;
+
+import parser.AST_Start;
+import parser.MiniSql;
+import parser.ParseException;
+import parser.TokenMgrError;
+import query.Optimizer;
+import query.Plan;
+import query.QueryException;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+
+public class MinibaseTest {
+
+  /** Default database file name. */
+  protected static String PATH = System.getProperty("user.name") + ".minibase";
+
+  /** Default database size (in pages). */
+  protected static int DB_SIZE = 10000;
+
+  /** Default buffer pool size (in pages) */
+  protected static int BUF_SIZE = 100;
+  
+  /**Default prefetch size */
+  protected static int PRE_SIZE = 10;
+
+  /** Command line prompt, when interactive. */
+  protected static String PROMPT = "\nMSQL> ";
+
+  private int allocs;
+  private int reads;
+  private int writes;
+
+  private MiniSql parser;
+  private AST_Start node;
+
+  @Before
+  public void setUp() {
+    // print the welcome message and load the database
+    System.out.println("Minibase SQL Utility 1.0");
+    if (new File(PATH).exists()) {
+      System.out.println("Loading database...");
+      new Minibase(PATH, DB_SIZE, BUF_SIZE, PRE_SIZE, "Clock", true);
+    } else {
+      System.out.println("Creating database...");
+      new Minibase(PATH, DB_SIZE, BUF_SIZE, PRE_SIZE, "Clock", false);
+    }
+
+    // initialize the performance counters
+    allocs = Minibase.DiskManager.getAllocCount();
+    reads = Minibase.DiskManager.getReadCount();
+    writes = Minibase.DiskManager.getWriteCount();
+
+    parser = new MiniSql(System.in);
+  }
+
+  @After
+  public void tearDown() {
+    // close the database and exit
+    System.out.println("Closing database...");
+    Minibase.DiskManager.closeDB();
+  }
+
+  @Test
+  public void testCreateIndex() {
+    Assert.assertTrue("Not implemented", true == false);
+  }
+}
