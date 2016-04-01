5f2864aae4e68bc3d919050f90036c80c3293f58
Elliott Mantock <emantock@gmail.com>
2016-04-01 05:48:01 -0400

    check for invalid table, col, values, or preds

--- src/query/Update.java
+++ src/query/Update.java
@@ -2,10 +2,20 @@ package query;
 
 import parser.AST_Update;
 
+import global.Minibase;
+import relop.Predicate;
+import relop.Schema;
+
 /**
  * Execution plan for updating tuples.
  */
 class Update implements Plan {
+
+  private String fileName;
+  private Predicate[][] predicates;
+  private String[] cols;
+  private Object[] values;
+  private Schema schema;
 
   /**
    * Optimizes the plan, given the parsed query.
@@ -13,7 +23,18 @@ class Update implements Plan {
    * @throws QueryException if invalid column names, values, or pedicates
    */
   public Update(AST_Update tree) throws QueryException {
+    this.fileName = tree.getFileName();
+    this.predicates = tree.getPredicates();
+    this.cols = tree.getColumns();
+    this.values = tree.getValues();
+    this.schema = Minibase.SystemCatalog.getSchema(fileName);
 
+    QueryCheck.tableExists(fileName);
+    QueryCheck.insertValues(schema, values);
+    QueryCheck.predicates(schema, predicates);
+    for (String col : cols) {
+      QueryCheck.columnExists(schema, col);
+    }
   } // public Update(AST_Update tree) throws QueryException
 
   /**
