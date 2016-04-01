33c4655adf9083a25b64e64dfeccf45a138c89cb
Elliott Mantock <emantock@gmail.com>
2016-04-01 15:21:18 -0400

    verify update and added one more test

--- src/tests/UpdateTest.java
+++ src/tests/UpdateTest.java
@@ -1,10 +1,15 @@
 package tests;
 
+import global.Minibase;
 import global.Msql;
+import heap.HeapFile;
+import relop.Tuple;
+import relop.FileScan;
 import parser.ParseException;
 import parser.TokenMgrError;
 import query.QueryException;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -37,7 +42,17 @@ public class UpdateTest extends MinibaseTest {
       e.printStackTrace();
     }
 
-    // TODO: Verify update
+    HeapFile file = new HeapFile("Students");
+    FileScan scan = new FileScan(Minibase.SystemCatalog.getSchema("Students"), file);
+
+    while (scan.hasNext()) {
+      Tuple t = scan.getNext();
+      if (t.getField("name").equals("Chris")) {
+        Assert.assertEquals("The sid for 'Chris' wasn't updated properly", 5, t.getField("sid"));
+      }
+    }
+
+    scan.close();
   }
 
   @Test
@@ -52,7 +67,44 @@ public class UpdateTest extends MinibaseTest {
       e.printStackTrace();
     }
 
-    // TODO: Verify update
+    HeapFile file = new HeapFile("Students");
+    FileScan scan = new FileScan(Minibase.SystemCatalog.getSchema("Students"), file);
+
+    while (scan.hasNext()) {
+      Tuple t = scan.getNext();
+      if (t.getField("name").equals("Chris")) {
+        Assert.assertEquals("The sid for 'Chris' wasn't updated properly", 5, t.getField("sid"));
+      }
+
+      if (t.getField("name").equals("Alice")) {
+        Assert.assertEquals("The sid for 'Alice' wasn't updated properly", 5, t.getField("sid"));
+      }
+    }
+
+    scan.close();
+  }
+
+  @Test
+  public void testUpdateNoRows() {
+    try {
+      Msql.execute("UPDATE Students SET sid = 10 WHERE name = 'Elliott';\nQUIT;");
+    } catch(ParseException e){
+      e.printStackTrace();
+    } catch(TokenMgrError e) {
+      e.printStackTrace();
+    } catch(QueryException e) {
+      e.printStackTrace();
+    }
+
+    HeapFile file = new HeapFile("Students");
+    FileScan scan = new FileScan(Minibase.SystemCatalog.getSchema("Students"), file);
+
+    while (scan.hasNext()) {
+      Tuple t = scan.getNext();
+      Assert.assertFalse("None of the SIDs should be 10", (int)t.getField("sid") == 10);
+    }
+
+    scan.close();
   }
 
   @Test (expected=QueryException.class)
