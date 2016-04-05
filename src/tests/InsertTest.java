a19142ee4045157c9a999c12800f7269b33bcb8a
Elliott Mantock <emantock@gmail.com>
2016-04-04 22:16:33 -0700

    check record statistics

--- src/tests/InsertTest.java
+++ src/tests/InsertTest.java
@@ -63,6 +63,30 @@ public class InsertTest extends MinibaseTest {
     Assert.assertTrue("No row with specified data was found.", passes);
   }
 
+  @Test
+  public void testRecordStatsUpdated() {
+    try {
+      Msql.execute("INSERT INTO Students VALUES (1, 'Alice', 25.67);\nINSERT INTO Students VALUES (2, 'Chris', 12.34);\nQUIT");
+    } catch(ParseException e){
+      e.printStackTrace();
+    } catch(TokenMgrError e) {
+      e.printStackTrace();
+    } catch(QueryException e) {
+      e.printStackTrace();
+    }
+
+    FileScan scan = new FileScan(Minibase.SystemCatalog.s_rel, Minibase.SystemCatalog.f_rel);
+    while (scan.hasNext()) {
+      Tuple t = scan.getNext();
+      if (t.getField("relName").equals("Students")) {
+        Assert.assertEquals("The record stats weren't updated properly", 2, (int)t.getField("recCount"));
+        break;
+      }
+    }
+
+    scan.close();
+  }
+
   @Test (expected=QueryException.class)
   public void testInsertBadRow() throws QueryException {
     try {
